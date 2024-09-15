ebccfbb4325e495f2d89503ba3bb37dded69aaff
Tushar <stuffcopresident@gmail.com>
2024-09-15 19:05:05 -0400

    Small changes

--- Randomizer.cs
+++ Randomizer.cs
@@ -1,3 +1,4 @@
+using RNSReloaded.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,34 +6,41 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RNSReloaded.Randomizer;
-public class Randomizer {
+
+//At some point I'm going to create a wrapper class over IRNSReloaded to have safe accessor
+public unsafe class Randomizer {
     private Dictionary<string, string> completeMap = [];
+    private Dictionary<string, string> languageMap = [];
 
     public Randomizer() {
 
     }
 
-    public void Randomize() {
+    public void Randomize(
+        IRNSReloaded rns
+        ) {
         //Get all the strings that need to be randomized
         //put them in a map ( I guess it could just be an array )
         //  optional: based on some filter
-        this.LoadLanguageMap();
-        this.LoadAllyData();
-        this.LoadItemData();
+        this.LoadLanguageMap(rns);
+        this.LoadAllyData(rns);
+        this.LoadItemData(rns);
         //shuffle said map
         //  optional: maybe this step is based on a filter
         //for each entry
         //reverse the map key to the data source
     }
-    public void LoadLanguageMap() {
+    private void LoadLanguageMap(IRNSReloaded rns) {
         //o h my god do it manually, then tidy it later
+        var global = rns.GetGlobalInstance();
+        var langMap = rns.FindValue(global, "languageMap");
     }
 
-    public void LoadAllyData() {
+    private void LoadAllyData(IRNSReloaded rns) {
 
     }
 
-    public void LoadItemData() {
+    private void LoadItemData(IRNSReloaded rns) {
 
     }
 }
