972f8dbf5f904a7fec6bdd1da101ecfbaf652d16
Tushar <stuffcopresident@gmail.com>
2024-11-30 16:51:46 -0500

    Add dev config property

--- Config/Config.cs
+++ Config/Config.cs
@@ -5,25 +5,7 @@ namespace RNSReloaded.Randomizer.Config;
 public class Config : Configurable<Config> {
 
     [Category("TextRandomizer"), DefaultValue(false)]
-    [Description("Checked if strings should only be swapped with strings of the same category")]
-    public bool RandomizeWithinCategory { get; set; } = true;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextEffects { get; set; } = true;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextHBS { get; set; } = false;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextNames { get; set; } = false;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextDesc { get; set; } = false;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextTitles { get; set; } = false;
-
-    [Category("TextRandomizer"), DefaultValue(true)]
-    public bool RandTextMenus {  get; set; } = false;
+    [Description("Remaps all text to show the string ids")]
+    public bool ShowStringIds { get; set; } = false;
 
 }
